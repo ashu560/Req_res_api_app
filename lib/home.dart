9857565af0dce5178cdb421be8c02e73873fb08a
Ashutosh <ashu.deshmukh560@gmail.com>
2023-06-01 23:30:46 +0530

    App Created

---
+++ lib/home.dart
@@ -0,0 +1,143 @@
+// ignore_for_file: prefer_const_constructors, unused_local_variable
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  late List<dynamic> newsList = [];
+
+  Future<void> apicall() async {
+    final response = await http.post(
+      Uri.parse(
+          'http://devapi.hidoc.co:8080/HidocWebApp/api/getArticlesByUid?sId=500&uuId=&userId=423914'),
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        final responseData = json.decode(response.body);
+        newsList = responseData.containsKey('data')
+            ? responseData['data']['trandingBulletin']
+            : [];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    apicall();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            // Need to give route
+          },
+          icon: Icon(
+            Icons.house_siding_outlined,
+            color: Colors.black,
+            size: 40,
+          ),
+        ),
+        centerTitle: true,
+        title: const Text(
+          "HiDoc App",
+          style: TextStyle(color: Colors.black),
+        ),
+        toolbarHeight: 150,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        scrolledUnderElevation: 20,
+      ),
+      body: Stack(children: [
+        Positioned(
+          top: -350,
+          left: -250,
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/bg.png', // Replace with your image path
+              fit: BoxFit.contain,
+              opacity: const AlwaysStoppedAnimation(0.5),
+            ),
+          ),
+        ),
+        ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          // controller: ScrollController(initialScrollOffset: 0.0),
+          itemCount: newsList.length,
+          itemBuilder: (context, index) {
+            final newsItem = newsList[index];
+            final articleImg =
+                newsItem['articleImg'] ?? ''; // Provide a default value if null
+            return Column(
+              children: [
+                // Image.network(
+                //   newsItem['articleImg'],
+                //   cacheHeight: 350,
+                //   cacheWidth: 350,
+                // ),
+                Container(
+                  height: 350,
+                  width: 350,
+                  padding: EdgeInsets.all(
+                    20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Text(
+                        newsItem['articleTitle'].toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 22,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        newsItem['articleDescription'].toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            );
+          },
+        ),
+      ]),
+    );
+  }
+}
